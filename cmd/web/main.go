a19fb00200eb35cb6b03b759f7429ea2882165e8
rmuell9 <rmatthewmueller@gmail.com>
2026-01-20 22:17:05 -0600

    stuff

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,59 @@
 package main
 
 import (
+	"flag"
+	"os"
 	"log"
 	"net/http"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("snippet/create", snippetCreate)
+type application struct {
+	errorLog *log.Logger
+	infoLog *log.Logger
+}
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	// Parses the command-line flag, giving a value to addr
+	flag.Parse()
+
+	// Loggers for making information and error messages
+	infoLog := log.New(
+		os.Stdout, 
+		"INFO\t", 
+		log.Ldate|log.Ltime)
+
+	errorLog := log.New(
+		os.Stderr, 
+		"ERROR\t", 
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
+
+	// Init servers
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Register the file server as handler for all URL paths with "/static/"
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Register application routes
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("snippet/create", app.snippetCreate)
+
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+
+	// Error-handling
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
