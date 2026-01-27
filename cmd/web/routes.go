a303fada01c5577a8e17d503ff2e32566c477d8c
rmuell9 <rmatthewmueller@gmail.com>
2026-01-27 16:15:27 -0600

    signup

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,8 +12,8 @@ func (app *application) routes() http.Handler {
 
 	router.NotFound = http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-		app.notFound(w)
-	}) 
+			app.notFound(w)
+		}) 
 
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -30,7 +30,12 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodPost, "/snippet/create",
 		dynamic.ThenFunc(app.snippetCreatePost))
 
-	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
+	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
+	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
+	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
+	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogoutPost))
 
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	return standard.Then(router)
 }
